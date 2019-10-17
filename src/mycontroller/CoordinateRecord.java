afa93d1d2d52377abe6def8db17c766022419a9e
leyiao <leyia@student.unimelb.edu.au>
2019-10-17 16:19:58 +1100

    CoordinateRecord

---
+++ src/mycontroller/CoordinateRecord.java
@@ -0,0 +1,20 @@
+package mycontroller;
+
+import tiles.MapTile;
+
+public class CoordinateRecord {
+	private boolean reachable;
+	private MapTile tile;
+	
+	public CoordinateRecord(MapTile tile) {
+		this.tile = tile;
+		this.reachable = true;
+	}
+	
+	public void setUnreachable() {
+		this.reachable = false;
+	}
+	public boolean getReachable() {
+		return this.reachable;
+	}
+}
